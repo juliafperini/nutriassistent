4d5e130330cca4d442366970d342c7a00ce5281e
Julia Perini <juliafperini@gmail.com>
2024-12-22 20:42:10 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nutriassistent',
+  webDir: 'www'
+};
+
+export default config;
